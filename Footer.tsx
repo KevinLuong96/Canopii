f3c021ff227cd08b47a82a4aafdaa4849e755be3
kevinluong96 <k5luong@uwaterloo.ca>
2019-11-19 17:29:12 -0500

    initial commit

---
+++ Footer.tsx
@@ -0,0 +1,20 @@
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const Footer = () => {
+    return(
+        <View>
+            <Text>Penis</Text>
+        </View>
+    );
+};
+
+export default Footer;
+
