3d546d9efc3c080b3b77374ef6c4f10c36c99ca4
kevinluong96 <k5luong@uwaterloo.ca>
2020-03-05 12:50:42 -0500

    add static folders

---
+++ leaves.ts
@@ -0,0 +1,25 @@
+export const leaves = {
+  acer_saccharum: {
+    lab: require("./images/leaves/acer_saccharum/lab.jpg"),
+    pic1: require("./images/leaves/acer_saccharum/pic1.jpg"),
+    pic2: require("./images/leaves/acer_saccharum/pic2.jpg"),
+    pic3: require("./images/leaves/acer_saccharum/pic3.jpg"),
+    pic4: require("./images/leaves/acer_saccharum/pic4.jpg"),
+    pic5: require("./images/leaves/acer_saccharum/pic5.jpg"),
+    pic6: require("./images/leaves/acer_saccharum/pic6.jpg"),
+  },
+  quercus_rubra: {
+    lab: require("./images/leaves/quercus_rubra/lab.png"),
+    pic1: require("./images/leaves/quercus_rubra/pic1.jpg"),
+    pic2: require("./images/leaves/quercus_rubra/pic2.jpg"),
+    pic3: require("./images/leaves/quercus_rubra/pic3.jpg"),
+    pic4: require("./images/leaves/quercus_rubra/pic4.jpg"),
+  },
+  tilia_americana: {
+    lab: require("./images/leaves/tilia_americana/lab.png"),
+    pic1: require("./images/leaves/tilia_americana/pic1.jpg"),
+    pic2: require("./images/leaves/tilia_americana/pic2.jpg"),
+    pic3: require("./images/leaves/tilia_americana/pic3.jpg"),
+    pic4: require("./images/leaves/tilia_americana/pic4.jpg"),
+  },
+};
