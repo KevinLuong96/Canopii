65da6d1fb347fcadafb57acc5507b0939518724a
kevinluong96 <k5luong@uwaterloo.ca>
2020-03-08 22:45:18 -0400

    tree images are in

--- leaves.ts
+++ leaves.ts
@@ -10,6 +10,162 @@ export const leaves = {
       require("./images/leaves/acer_saccharum/pic6.jpg"),
     ],
   },
+  acer_ginnala: {
+    lab: require("./images/leaves/acer_ginnala/lab.png"),
+    pics: [
+      require("./images/leaves/acer_ginnala/pic1.jpg"),
+      require("./images/leaves/acer_ginnala/pic2.jpg"),
+      require("./images/leaves/acer_ginnala/pic3.png"),
+      require("./images/leaves/acer_ginnala/pic4.jpeg"),
+    ],
+  },
+  acer_negundo: {
+    lab: require("./images/leaves/acer_negundo/lab.jpg"),
+    pics: [
+      require("./images/leaves/acer_negundo/pic1.jpg"),
+      require("./images/leaves/acer_negundo/pic2.jpg"),
+      require("./images/leaves/acer_negundo/pic3.jpg"),
+      require("./images/leaves/acer_negundo/pic4.jpg"),
+    ],
+  },
+  acer_platanoides: {
+    lab: require("./images/leaves/acer_platanoides/lab.jpg"),
+    pics: [
+      require("./images/leaves/acer_platanoides/pic1.jpg"),
+      require("./images/leaves/acer_platanoides/pic2.jpg"),
+      require("./images/leaves/acer_platanoides/pic3.jpg"),
+    ],
+  },
+  acer_rubrum: {
+    lab: require("./images/leaves/acer_rubrum/lab.jpg"),
+    pics: [
+      require("./images/leaves/acer_rubrum/pic1.jpg"),
+      require("./images/leaves/acer_rubrum/pic2.jpeg"),
+      require("./images/leaves/acer_rubrum/pic3.jpg"),
+      require("./images/leaves/acer_rubrum/pic4.jpg"),
+      require("./images/leaves/acer_rubrum/pic5.jpg"),
+    ],
+  },
+  acer_saccharinum: {
+    lab: require("./images/leaves/acer_saccharinum/lab.png"),
+    pics: [
+      require("./images/leaves/acer_saccharinum/pic1.jpg"),
+      require("./images/leaves/acer_saccharinum/pic2.jpeg"),
+      require("./images/leaves/acer_saccharinum/pic3.jpg"),
+    ],
+  },
+  amelanchier_canadensis: {
+    lab: require("./images/leaves/amelanchier_canadensis/lab.png"),
+    pics: [
+      require("./images/leaves/amelanchier_canadensis/pic1.jpg"),
+      require("./images/leaves/amelanchier_canadensis/pic2.jpg"),
+      require("./images/leaves/amelanchier_canadensis/pic3.jpg"),
+      require("./images/leaves/amelanchier_canadensis/pic4.jpg"),
+    ],
+  },
+  betula_pendula: {
+    lab: require("./images/leaves/betula_pendula/lab.png"),
+    pics: [
+      require("./images/leaves/betula_pendula/pic1.jpg"),
+      require("./images/leaves/betula_pendula/pic2.jpeg"),
+      require("./images/leaves/betula_pendula/pic3.jpg"),
+      require("./images/leaves/betula_pendula/pic4.jpg"),
+    ],
+  },
+  celtis_occidentalis: {
+    lab: require("./images/leaves/celtis_occidentalis/lab.png"),
+    pics: [
+      require("./images/leaves/celtis_occidentalis/pic1.jpg"),
+      require("./images/leaves/celtis_occidentalis/pic2.jpg"),
+      require("./images/leaves/celtis_occidentalis/pic3.jpg"),
+      require("./images/leaves/celtis_occidentalis/pic4.jpg"),
+    ],
+  },
+  elaeagnus_angustifolia: {
+    lab: require("./images/leaves/elaeagnus_angustifolia/lab.png"),
+    pics: [
+      require("./images/leaves/elaeagnus_angustifolia/pic1.jpg"),
+      require("./images/leaves/elaeagnus_angustifolia/pic2.jpg"),
+    ],
+  },
+  fraxinus_americana: {
+    lab: require("./images/leaves/fraxinus_americana/lab.png"),
+    pics: [
+      require("./images/leaves/fraxinus_americana/pic1.jpg"),
+      require("./images/leaves/fraxinus_americana/pic2.jpg"),
+      require("./images/leaves/fraxinus_americana/pic3.jpg"),
+    ],
+  },
+  fraxinus_pennsylvanica: {
+    lab: require("./images/leaves/fraxinus_pennsylvanica/lab.png"),
+    pics: [
+      require("./images/leaves/fraxinus_pennsylvanica/pic1.jpg"),
+      require("./images/leaves/fraxinus_pennsylvanica/pic2.jpeg"),
+      require("./images/leaves/fraxinus_pennsylvanica/pic3.jpg"),
+      require("./images/leaves/fraxinus_pennsylvanica/pic4.jpeg"),
+    ],
+  },
+  ginkgo_biloba: {
+    lab: require("./images/leaves/ginkgo_biloba/lab.png"),
+    pics: [
+      require("./images/leaves/ginkgo_biloba/pic1.png"),
+      require("./images/leaves/ginkgo_biloba/pic2.png"),
+      require("./images/leaves/ginkgo_biloba/pic3.png"),
+      require("./images/leaves/ginkgo_biloba/pic4.png"),
+      require("./images/leaves/ginkgo_biloba/pic5.png"),
+    ],
+  },
+  gleditsia_triacanthos: {
+    lab: require("./images/leaves/gleditsia_triacanthos/lab.png"),
+    pics: [
+      require("./images/leaves/gleditsia_triacanthos/pic1.jpg"),
+      require("./images/leaves/gleditsia_triacanthos/pic2.jpg"),
+      require("./images/leaves/gleditsia_triacanthos/pic3.jpg"),
+      require("./images/leaves/gleditsia_triacanthos/pic4.jpg"),
+    ],
+  },
+  juglans_nigra: {
+    lab: require("./images/leaves/juglans_nigra/lab.png"),
+    pics: [
+      require("./images/leaves/juglans_nigra/pic1.jpg"),
+      require("./images/leaves/juglans_nigra/pic2.jpg"),
+      require("./images/leaves/juglans_nigra/pic3.jpg"),
+      require("./images/leaves/juglans_nigra/pic4.jpg"),
+    ],
+  },
+  malus_coronaria: {
+    lab: require("./images/leaves/malus_coronaria/lab.png"),
+    pics: [
+      require("./images/leaves/malus_coronaria/pic1.jpg"),
+      require("./images/leaves/malus_coronaria/pic2.jpg"),
+      require("./images/leaves/malus_coronaria/pic3.jpg"),
+    ],
+  },
+  pyrus_calleryana: {
+    lab: require("./images/leaves/pyrus_calleryana/lab.png"),
+    pics: [
+      require("./images/leaves/pyrus_calleryana/pic1.jpg"),
+      require("./images/leaves/pyrus_calleryana/pic2.jpg"),
+      require("./images/leaves/pyrus_calleryana/pic3.jpg"),
+    ],
+  },
+  quercus_macrocarpa: {
+    lab: require("./images/leaves/quercus_macrocarpa/lab.png"),
+    pics: [
+      require("./images/leaves/quercus_macrocarpa/pic1.jpg"),
+      require("./images/leaves/quercus_macrocarpa/pic2.jpg"),
+      require("./images/leaves/quercus_macrocarpa/pic3.jpg"),
+      require("./images/leaves/quercus_macrocarpa/pic4.jpg"),
+    ],
+  },
+  quercus_robur: {
+    lab: require("./images/leaves/quercus_robur/lab.png"),
+    pics: [
+      require("./images/leaves/quercus_robur/pic1.jpg"),
+      require("./images/leaves/quercus_robur/pic2.jpg"),
+      require("./images/leaves/quercus_robur/pic3.jpg"),
+    ],
+  },
   quercus_rubra: {
     lab: require("./images/leaves/quercus_rubra/lab.png"),
     pics: [
@@ -19,6 +175,32 @@ export const leaves = {
       require("./images/leaves/quercus_rubra/pic4.jpg"),
     ],
   },
+  salix_amygdaloides: {
+    lab: require("./images/leaves/salix_amygdaloides/lab.png"),
+    pics: [
+      require("./images/leaves/salix_amygdaloides/pic1.jpg"),
+      require("./images/leaves/salix_amygdaloides/pic2.jpg"),
+      require("./images/leaves/salix_amygdaloides/pic3.jpg"),
+    ],
+  },
+  sorbus_aucuparia: {
+    lab: require("./images/leaves/sorbus_aucuparia/lab.png"),
+    pics: [
+      require("./images/leaves/sorbus_aucuparia/pic1.jpg"),
+      require("./images/leaves/sorbus_aucuparia/pic2.jpg"),
+      require("./images/leaves/sorbus_aucuparia/pic3.png"),
+      require("./images/leaves/sorbus_aucuparia/pic4.jpg"),
+    ],
+  },
+  syringa_reticulata: {
+    lab: require("./images/leaves/syringa_reticulata/lab.png"),
+    pics: [
+      require("./images/leaves/syringa_reticulata/pic1.jpg"),
+      require("./images/leaves/syringa_reticulata/pic2.jpg"),
+      require("./images/leaves/syringa_reticulata/pic3.jpg"),
+      require("./images/leaves/syringa_reticulata/pic4.jpg"),
+    ],
+  },
   tilia_americana: {
     lab: require("./images/leaves/tilia_americana/lab.png"),
     pics: [
@@ -26,6 +208,24 @@ export const leaves = {
       require("./images/leaves/tilia_americana/pic2.jpg"),
       require("./images/leaves/tilia_americana/pic3.jpg"),
       require("./images/leaves/tilia_americana/pic4.jpg"),
+    ],
+  },
+  tilia_cordata: {
+    lab: require("./images/leaves/tilia_cordata/lab.png"),
+    pics: [
+      require("./images/leaves/tilia_cordata/pic1.jpg"),
+      require("./images/leaves/tilia_cordata/pic2.jpg"),
+      require("./images/leaves/tilia_cordata/pic3.jpg"),
+      require("./images/leaves/tilia_cordata/pic4.jpg"),
+    ],
+  },
+  ulmus_pumila: {
+    lab: require("./images/leaves/ulmus_pumila/lab.png"),
+    pics: [
+      require("./images/leaves/ulmus_pumila/pic1.jpg"),
+      require("./images/leaves/ulmus_pumila/pic2.jpg"),
+      require("./images/leaves/ulmus_pumila/pic3.jpg"),
+      require("./images/leaves/ulmus_pumila/pic4.jpg"),
     ],
   },
 };
